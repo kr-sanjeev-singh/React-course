ac19dfe7782ff55dc176df9eaa77addaee4517b1
Sanjeev Kumar Singh <sanjeev.singh.18@mountblue.tech>
2023-07-02 11:23:33 +0530

    Learn and revse vite, intial rerender and rerenders, general rule of hooks, and bactchng

--- 03-advanced-react/src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
+
 const UseStateBasics = () => {
-  return <h2>useState basics</h2>;
+    const [count, setCount] = useState(0);
+
+    const handleClick = () => {
+        setCount(count + 1);
+    };
+    return (
+        <div>
+            <h2>You clicked {count} times</h2>
+            <button type="button" className="btn" onClick={handleClick}>
+                increase
+            </button>
+        </div>
+    );
 };
 
 export default UseStateBasics;
--- 03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,24 @@
+import { useState } from "react";
+
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+    const [count, setCount] = useState(0);
+
+    const handleClick = () => {
+        setTimeout(() => {
+            console.log("button click");
+            setCount((prev) => prev + 1);
+        }, 3000);
+    };
+
+    return (
+        <div>
+            <h2>useState "gotcha"</h2>
+            <h1>{count}</h1>
+            <button type="button" className="btn" onClick={handleClick}>
+                increase
+            </button>
+        </div>
+    );
 };
 
 export default UseStateGotcha;
